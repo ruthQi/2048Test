b14601affdeeb300dc22610b4ffcc1fb19f142e3
qiru <bjqiru@corp.netease.com>
2018-01-22 15:01:47 +0800

    添加结构

--- routes/init.js
+++ routes/init.js
@@ -2,6 +2,10 @@
 var index = require('./pages/index');
 
 module.exports = function(app) {
-    app.use('/', index);
+   app.use('/', index);
+
+   app.use('/2048', function(req, res, next){
+      res.render('pages/2048');
+   })
 
 };
